8e00dff9b64998ebdb62bd15cb64c1a7176bf33b
Ricardo Gonçalves <ricardo@mytiki.com>
2022-04-26 18:15:29 -0300

    publish

--- lib/microsoft_provider.dart
+++ lib/microsoft_provider.dart
@@ -3,7 +3,6 @@ import 'package:httpp/httpp.dart';
 
 import 'src/microsoft_provider_service.dart';
 import 'src/model/email/microsoft_provider_model_email.dart';
-import 'src/model/info/microsoft_provider_info_model.dart';
 import 'src/model/microsoft_provider_model.dart';
 
 export 'src/model/microsoft_provider_model.dart';
